1c66ec2e9e8fba4ee032ae5371734d758ce6e42d
Pragya Maheshwari <143798085+prags85@users.noreply.github.com>
2025-01-09 01:17:37 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit.runtime.scriptrunner.script_run_context import get_script_run_ctx
+
 
 # tag::write_message[]
 def write_message(role, content, save = True):
@@ -16,7 +16,5 @@ def write_message(role, content, save = True):
         st.markdown(content)
 # end::write_message[]
 
-# tag::get_session_id[]
-def get_session_id():
-    return get_script_run_ctx().session_id
+
 # end::get_session_id[]
